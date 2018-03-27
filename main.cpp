61785bcb41c45c8e2121667bd94b17008089aac9
Alessandro <wansener@hotmaail.com>
2018-03-26 20:45:34 -0400

    Copy-constructor, cout operator

--- main.cpp
+++ main.cpp
@@ -5,17 +5,29 @@ private:
     double *m;
     int rows, cols;
 public:
-    /*
-        0 1 2 3
-    4 5 6 7
-        8 9 10 11
-     */
+    Matrix(const Matrix &orig) : rows(orig.rows), cols(orig.cols), m(new double[rows][cols]) {
+        for (int i = 0; i < rows; i++){
+            for (int j = 0; j < cols; j++) {
+                m[i] = orig.m[i];
+            }
+        }
+    }
+/*
+    0 1 2 3
+4 5 6 7
+    8 9 10 11
+ */
     double& 	operator ()(int i, int j)  {
         return m[i*cols + j];
     }
     double 	operator ()(int i, int j)const {
         return m[i*cols + j];
     }
+    friend ostream& operator <<(ostream& s, const Matrix& mat){
+        for (int i = 0; i < mat.rows; i++){
+            for (int j = 0; j < mat.cols; j++) {
+               s << mat.m[i];
+            }
 };
 
 
